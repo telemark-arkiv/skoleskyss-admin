8ec344c2c056732fe1ca0769ae298704ddd7250c
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-07-04 15:40:41 +0200

    Adds support for admin groups

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ module.exports = {
   YAR_SECRET: process.env.YAR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || 'https://auth.ldap.tjeneste.win',
   LOGS_SERVICE_URL: process.env.LOGS_SERVICE_URL || 'https://logs.skoleskyss.win',
+  USER_SERVICE_URL: process.env.USER_SERVICE_URL || 'https://auth.demo.t-fk.win/lookup',
+  ACCESS_GROUP: process.env.ACCESS_GROUP || 'TFK-TG-SkoleskyssAdmin',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'skoleskyss-dev',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
--- handlers/auth.js
+++ handlers/auth.js
@@ -1,27 +1,37 @@
 'use strict'
 
 const config = require('../config')
-const verifySigninJwt = require('../lib/verify-signin-jwt')
+const verifyJwt = require('../lib/verify-jwt')
+const lookupUser = require('../lib/lookup-user')
 const logger = require('../lib/logger')
 
 module.exports.doSignIn = async (request, reply) => {
   const token = request.query.jwt
   const nextPath = request.query.nextPath
-  try {
-    const user = await verifySigninJwt(token)
+  const yar = request.yar
+  const check = await verifyJwt(token)
+  if (check.isValid === true) {
+    logger('info', ['auth', 'doSignIn', 'token is valid'])
+    try {
+      const user = await lookupUser(token)
+      logger('info', ['auth', 'doSignIn', 'user verified', 'userId', user.userId])
 
-    logger('info', ['auth', 'doSignIn', 'verified', user.userId])
+      yar.set('isAdmin', user.isAdmin)
 
-    request.cookieAuth.set({data: user, token: token})
+      request.cookieAuth.set({data: user, token: token})
 
-    if (nextPath && nextPath.length > 0) {
-      reply.redirect(nextPath)
-    } else {
-      reply.redirect('/')
+      if (nextPath && nextPath.length > 0) {
+        reply.redirect(nextPath)
+      } else {
+        reply.redirect('/')
+      }
+    } catch (error) {
+      logger('error', ['auth', 'doSignIn', 'error', error])
+      reply(error)
     }
-  } catch (error) {
-    logger('error', ['auth', 'doSignIn', error])
-    reply(error)
+  } else {
+    logger('error', ['auth', 'doSignIn', 'invalid token'])
+    reply(new Error('Invalid token'))
   }
 }
 
